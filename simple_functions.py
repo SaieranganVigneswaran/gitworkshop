20a42b1de4df8e882080b5bb3434b22ec6887f60
SaieranganVigneswaran <saierangan.vigne@tigeranalytics.com>
2024-03-21 11:54:13 +0530

    done

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,9 @@
 # Custom python functions
-
+# Doubles the given numbers
 def double_number(a):
-    return a+a
+    return a + a
 
+
+# Squares the given number
 def square_number(a):
-    return a*a
+    return a * a
